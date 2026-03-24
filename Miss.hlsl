777dce43ede1bf5f6ee0158d537f74e0dae74e48
Heather Carter <heathercarter2006@outlook.com>
2026-03-24 10:26:39 +0000

    Gradient in miss shader

--- Miss.hlsl
+++ Miss.hlsl
@@ -4,7 +4,21 @@
                            : SV_RayPayload) {
     
     float3 rayDir = WorldRayOrigin();
+    float3 rayDimension = DispatchRaysDimensions();
     
-    float compressed = ((rayDir.y + 1 * 0.5f));
-    payload.colorAndDistance = float4(compressed, 0.0f, 0.0f, 1);
+    uint2 launchIndex = DispatchRaysIndex().xy;
+    float2 dims = float2(DispatchRaysDimensions().xy);
+    
+    float3 blue = float3(0, 1, 0);
+    float3 red = float3(1, 0, 0);
+    float3 purple = float3(1, 0, 1);
+    
+    float ramp =  saturate(launchIndex.y / dims.y);
+    float3 colour = lerp(red, blue, ramp);
+    
+    ramp =  saturate(launchIndex.x / dims.x);
+    float3 colour2 = lerp(colour, purple, ramp);
+    
+    
+    payload.colorAndDistance = float4(colour2, 1);
 }
